794b50bea11c11b2fd449610f7a4228ee363a754
DrBrown28 <112201610+DrBrown28@users.noreply.github.com>
2024-04-02 23:31:16 -0400

    zsmalloc: fix races between modifications of fullness and isolated
    We encountered many kernel exceptions of VM_BUG_ON(zspage->isolated ==
    0) in dec_zspage_isolation() and BUG_ON(!pages[1]) in zs_unmap_object()
    lately.  This issue only occurs when migration and reclamation occur at
    the same time.
    
    With our memory stress test, we can reproduce this issue several times
    a day.  We have no idea why no one else encountered this issue.  BTW,
    we switched to the new kernel version with this defect a few months
    ago.
    
    Since fullness and isolated share the same unsigned int, modifications of
    them should be protected by the same lock.
    
    [andrew.yang@mediatek.com: move comment]
      Link: https://lkml.kernel.org/r/20230727062910.6337-1-andrew.yang@mediatek.com
    Link: https://lkml.kernel.org/r/20230721063705.11455-1-andrew.yang@mediatek.com
    Fixes: c4549b871102 ("zsmalloc: remove zspage isolation for migration")
    Signed-off-by: Andrew Yang <andrew.yang@mediatek.com>
    Reviewed-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Cc: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Cc: Matthias Brugger <matthias.bgg@gmail.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    [Kazuki: Backport to v5.4]
    Signed-off-by: Kazuki Hashimoto <kazukih0205@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -286,6 +286,7 @@ struct zspage {
 	unsigned int freeobj;
 	struct page *first_page;
 	struct list_head list; /* fullness list */
+	struct zs_pool *pool;
 #ifdef CONFIG_COMPACTION
 	rwlock_t lock;
 #endif
@@ -1112,6 +1113,7 @@ static struct zspage *alloc_zspage(struct zs_pool *pool,
 
 	create_page_chain(class, zspage, pages);
 	init_zspage(class, zspage);
+	zspage->pool = pool;
 
 	return zspage;
 }
@@ -2049,11 +2051,12 @@ static int zs_page_migrate(struct address_space *mapping, struct page *newpage,
 	VM_BUG_ON_PAGE(!PageIsolated(page), page);
 
 	zspage = get_zspage(page);
+	pool = zspage->pool;
 
 	/* Concurrent compactor cannot migrate any subpage in zspage */
-	migrate_write_lock(zspage);
+	pool = zspage->pool;
+	spin_lock(&pool->lock);
 	get_zspage_mapping(zspage, &class_idx, &fullness);
-	pool = mapping->private_data;
 	class = pool->size_class[class_idx];
 	offset = get_first_obj_offset(page);
 
@@ -2145,7 +2148,7 @@ unpin_objects:
 	}
 	kunmap_atomic(s_addr);
 	spin_unlock(&class->lock);
-	migrate_write_unlock(zspage);
+        spin_unlock(&pool->lock);
 
 	return ret;
 }
@@ -2163,6 +2166,8 @@ static void zs_page_putback(struct page *page)
 	VM_BUG_ON_PAGE(!PageIsolated(page), page);
 
 	zspage = get_zspage(page);
+	pool = zspage->pool;
+	spin_lock(&pool->lock);
 	get_zspage_mapping(zspage, &class_idx, &fg);
 	mapping = page_mapping(page);
 	pool = mapping->private_data;
@@ -2170,6 +2175,7 @@ static void zs_page_putback(struct page *page)
 
 	spin_lock(&class->lock);
 	dec_zspage_isolation(zspage);
+	spin_unlock(&pool->lock);
 	if (!is_zspage_isolated(zspage)) {
 		/*
 		 * Due to page_lock, we cannot free zspage immediately
